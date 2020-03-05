882e652d5e57f82f27da9def4a6ae8a778f5a8a5
danielwtj <danielwtj@yahoo.com>
2020-03-05 13:06:01 -0800

    DetailScreen updated, cleaned StyleSheet

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,17 +1,20 @@
-import React, { useState } from 'react'
-import { View, Text } from 'react-native'
+import React from 'react'
+import { View, Text, Image, Dimensions } from 'react-native'
 import { material } from 'react-native-typography'
 import styles from '../style/style'
+import { Divider } from 'react-native-elements'
 
 export default function DetailScreen(props) {
-    const [recipe, setRecipe] = useState(props.navigation.getParam('recipe'))
+    const recipe = props.navigation.getParam('recipe')
     return (
         <View style={styles.container}>
-            <View style={[styles.whiteRoundedBox, styles.dropShadow]}>
+            <Image source={require('../assets/comingsoon.jpg')} style={{width: Dimensions.get('window').width, height: Dimensions.get('window').width*.66}} />
+            <View style={{paddingHorizontal: 20}}>
                 <Text style={material.headline}>Ingredients:</Text>
                 {recipe.ingredients.map((ingredient) => <View key={ingredient.name} style={{flexDirection: 'row', alignItems: 'center'}}><Text style={material.body2}>{ingredient.quantity} {ingredient.unit} </Text><Text style={material.body1}>{ingredient.name}</Text></View>)}    
             </View>
-            <View style={[styles.whiteRoundedBox, styles.dropShadow]}>
+            <Divider style={{marginVertical: 10}}/>
+            <View style={{paddingHorizontal: 20}}>
                 <Text style={material.headline}>Instructions:</Text>
                 <Text style={material.body1}>{recipe.instructions}</Text>
             </View>
@@ -21,7 +24,4 @@ export default function DetailScreen(props) {
 
 DetailScreen.navigationOptions = ({navigation}) => ({
     title: navigation.getParam('recipe').title,
-    headerStyle: {backgroundColor: '#f8debd', shadowColor: 'transparent'},
-    headerTintColor: '#f8f8f2',
-    headerTitleStyle: material.headlineWhite
 })
--- style/style.js
+++ style/style.js
@@ -1,7 +1,5 @@
-import { StyleSheet, Dimensions } from 'react-native'
+import { StyleSheet } from 'react-native'
 import Constants from 'expo-constants'
-
-const TILESIZE = Dimensions.get('window').width * .4
 
 const COLORS = {
     offWhite: '#fafafa',
@@ -18,45 +16,10 @@ export default StyleSheet.create({
     listItemCell: {
         backgroundColor: COLORS.offWhite,
     },
-    searchContainer: {
-        backgroundColor: '#f8f8f2',
-        borderRadius: 10,
-        marginRight: 5,
-        paddingVertical: 2,
-        flex: 1,
-    },
-    searchInputContainer: {
-        borderBottomWidth: 0,
-    },
-    filterButton: {
-        backgroundColor: '#f8f8f2',
-        borderRadius: 10,
-        padding: 5,
-        marginLeft: 5,
-    },
-    searchFilterRow: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginHorizontal: 15,
-    },
     dropShadow: {
         shadowColor: '#ccc',
         shadowOffset: {width: 3, height: 3},
         shadowRadius: 3,
         shadowOpacity: 0.7,
-    },
-    whiteRoundedBox: {
-        backgroundColor: '#f8f8f2',
-        margin: 10,
-        borderRadius: 10,
-        padding: 10,
-    },
-    doubleCenter: {
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    tileSize: {
-        width: TILESIZE,
-        height: TILESIZE,
     },
 })
